288ad318b6a572a25bd563037b4e8ca16ca2aea7
Jon Schlinkert <jon.schlinkert@sellside.com>
2025-04-18 21:56:44 -0400

    examples

--- examples/example.ts
+++ examples/example.ts
@@ -1,6 +1,48 @@
-import { parse } from '../src/index';
+import util from 'node:util';
+import { parse } from '~/parse';
 
-const tree = parse(/^a(?<foo>z)[a-b]$/);
+const regex = /^a(?<foo>z)[a-b]$/;
+const tree = parse(regex);
 
-console.log(tree);
+console.log(regex);
+console.log(util.inspect(tree, { depth: null, colors: true }));
+// {
+//   type: 'root',
+//   value: '/^a(?<foo>z)[a-b]$/',
+//   nodes: [
+//     { type: 'slash', value: '/' },
+//     { type: 'caret', value: '^' },
+//     { type: 'text', value: 'a' },
+//     {
+//       type: 'paren',
+//       value: '(?<foo>z)',
+//       nodes: [
+//         { type: 'left_paren', value: '(' },
+//         { type: 'qmark', value: '?' },
+//         {
+//           type: 'angle',
+//           value: '<foo>',
+//           nodes: [
+//             { type: 'left_angle', value: '<' },
+//             { type: 'text', value: 'foo' },
+//             { type: 'right_angle', value: '>' }
+//           ]
+//         },
+//         { type: 'text', value: 'z' },
+//         { type: 'right_paren', value: ')' }
+//       ]
+//     },
+//     {
+//       type: 'bracket',
+//       value: '[a-b]',
+//       nodes: [
+//         { type: 'left_bracket', value: '[' },
+//         { type: 'text', value: 'a-b' },
+//         { type: 'right_bracket', value: ']' }
+//       ]
+//     },
+//     { type: 'dollar', value: '$' },
+//     { type: 'slash', value: '/' }
+//   ]
+// }
 console.log(tree.print());
